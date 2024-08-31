c907b76710b4c8e24f05c460df90afdd1edbbbb4
Colin Anderson <c.anderson1830@gmail.com>
2024-08-31 14:21:55 -0500

    UnitsEnum

---
+++ UnitsEnum.py
@@ -0,0 +1,20 @@
+from enum import Enum
+
+class Units(Enum):
+    Infantry=0
+    MechInfantry=1
+    Artillery=2
+    Tank=3
+    Fighter=4
+    TacticalBomber=5
+    StratBomber=6
+    Submarine=7
+    Destroyer=8
+    Cruiser=9
+    Battleship=10
+    Carrier=11
+    Transport=12
+    InfArt=13
+    MechInfArt=14
+    TankTactBomber=15
+    FighterTactBomber=16
