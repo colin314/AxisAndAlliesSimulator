9f42704d26d0d56b1cf37ca347e69177fcd2c3af
Colin Anderson <c.anderson1830@gmail.com>
2024-09-01 21:19:22 -0500

    implement lt function

--- Hit.py
+++ Hit.py
@@ -15,3 +15,15 @@ class Hit:
         isVulnerable = any(issubclass(unitType, vType) for vType in self.Vulnerable)
         isImmune = any(issubclass(unitType, iType) for iType in self.Immune)
         return isVulnerable and not isImmune
+    
+    # There is likely a bug here. This method is really janky (weird behavior if there is more than one entry in the vulnerable lists)
+    def __lt__(self, that):
+        cmpVal = None
+        for type in self.Vulnerable:
+            for type2 in that.Vulnerable:
+                cmpVal = True if issubclass(type,type2) else cmpVal
+                cmpVal = False if issubclass(type2, type) else cmpVal
+        # If specified vulnerabilities are not subclasses of either, then prioritize hits with immunities.
+        if cmpVal == None:
+            cmpVal = len(self.Immune) < len(that.Immune)
+        return cmpVal
